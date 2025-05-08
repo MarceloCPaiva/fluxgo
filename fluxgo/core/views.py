b819c941195bf1dc415e0939fafebbea1b57ee0d
Marcelo Paiva <marcelochagas.paiva@gmail.com>
2025-05-07 23:42:25 -0300

    Criando as Views com Classe, definindo as rotas e criando a home.html

--- fluxgo/core/views.py
+++ fluxgo/core/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class HomeView(TemplateView):
+    """
+    View responsável por renderizar a página inicial do site institucional.
+
+    Utiliza TemplateView da biblioteca genérica de views do Django,
+    permitindo associar um template HTML sem a necessidade de lógica adicional.
+
+    URL associada: /
+    Template utilizado: core/home.html
+    """
+    
+    template_name = 'core/home.html'
